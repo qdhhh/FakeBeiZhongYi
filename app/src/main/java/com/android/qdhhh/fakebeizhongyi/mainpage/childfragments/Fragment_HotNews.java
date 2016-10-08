128f97ca36690441141a027d70c92c0904278531
qdhhh <39368719@qq.com>
2016-10-08 12:02:46 +0800

    2016-10-8 12:02

--- app/src/main/java/com/android/qdhhh/fakebeizhongyi/mainpage/childfragments/Fragment_HotNews.java
+++ app/src/main/java/com/android/qdhhh/fakebeizhongyi/mainpage/childfragments/Fragment_HotNews.java
@@ -5,13 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,7 +21,8 @@ import com.android.qdhhh.fakebeizhongyi.commom.LocalInfo;
 import com.android.qdhhh.fakebeizhongyi.commom.UrlAddress;
 import com.android.qdhhh.fakebeizhongyi.mainpage.Activity_News_Detail;
 import com.android.qdhhh.fakebeizhongyi.mainpage.bean.Main_News_Head_Bean;
-import com.bumptech.glide.Glide;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.squareup.picasso.Picasso;
 import com.youth.banner.Banner;
 import com.youth.banner.BannerConfig;
@@ -38,13 +39,17 @@ import java.util.List;
 
 public class Fragment_HotNews extends Fragment {
 
-    private RecyclerView main_mainpage_hotnews_rv_id;
-    private SwipeRefreshLayout main_mainpage_hotnews_srl_id;
+    private PullToRefreshListView main_mainpage_hotnews_ptrlv_id;
+
+    private MainpageOnRefreshListener mainpageOnRefreshListener;
+
+    private MainPageHotNewsAdapter mainPageHotNewsAdapter;
+
+    private ListView listView;
+
     private View view;
 
     private int picCount;
-
-    private List<ImageView> listImage;
 
     private View headView;
 
@@ -60,6 +65,8 @@ public class Fragment_HotNews extends Fragment {
 
     private List<Main_News_Head_Bean> mainNews_List;
 
+    private int pageCount = 1;
+
 
     public Fragment_HotNews() {
 
@@ -67,16 +74,12 @@ public class Fragment_HotNews extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         kjhttp = new KJHttp();
-
-        getNewsData();
-
         super.onCreate(savedInstanceState);
     }
 
     /**
-     * 网络请求下载新闻数据
+     * 网络请求下载新的新闻数据
      */
     private void getNewsData() {
         HttpParams params = new HttpParams();
@@ -93,6 +96,7 @@ public class Fragment_HotNews extends Fragment {
                 try {
                     String code = new JSONObject(t).getJSONObject("result").getString("error_code");
                     if ("200".equals(code)) {
+                        pageCount = 2;
                         setNewsData(t);
                     }
                 } catch (JSONException e) {
@@ -110,7 +114,9 @@ public class Fragment_HotNews extends Fragment {
 
             @Override
             public void onFinish() {
-                main_mainpage_hotnews_srl_id.setRefreshing(false);
+                if (main_mainpage_hotnews_ptrlv_id.isRefreshing()) {
+                    main_mainpage_hotnews_ptrlv_id.onRefreshComplete();
+                }
                 super.onFinish();
             }
         });
@@ -137,23 +143,21 @@ public class Fragment_HotNews extends Fragment {
             mainNews_List = JSONArray.parseArray(new JSONObject(json).getString("listnews"),
                     Main_News_Head_Bean.class);
 
-            setHeadView();
+            setHeadData();
 
-            setRecyclerView();
-
+            mainPageHotNewsAdapter.notifyDataSetChanged();
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment__hotnews, null);
-        main_mainpage_hotnews_rv_id = (RecyclerView) view.findViewById(R.id.main_mainpage_hotnews_rv_id);
-        main_mainpage_hotnews_srl_id = (SwipeRefreshLayout) view.findViewById(R.id.main_mainpage_hotnews_srl_id);
+        main_mainpage_hotnews_ptrlv_id = (PullToRefreshListView) view.findViewById(R.id.main_mainpage_hotnews_ptrlv_id);
+        listView = main_mainpage_hotnews_ptrlv_id.getRefreshableView();
         return view;
     }
 
@@ -161,13 +165,21 @@ public class Fragment_HotNews extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 
+        mainNews_Head = new LinkedList<>();
 
-        main_mainpage_hotnews_srl_id.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                getNewsData();
-            }
-        });
+        mainNews_List = new LinkedList<>();
+
+        mainpageOnRefreshListener = new MainpageOnRefreshListener();
+
+        mainPageHotNewsAdapter = new MainPageHotNewsAdapter();
+
+        setHeadView();
+
+        listView.addHeaderView(headView);
+
+        setListView();
+
+        getNewsData();
 
         super.onActivityCreated(savedInstanceState);
     }
@@ -182,14 +194,6 @@ public class Fragment_HotNews extends Fragment {
         mainpage_hotnews_vp_id = (Banner) headView.findViewById(R.id.mainpage_hotnews_vp_id);
 
         mainpage_hotnews_vp_id.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
-
-        setHeadData();
-
-        mainpage_hotnews_vp_id.setImages(headURL);
-
-        mainpage_hotnews_vp_id.setBannerTitleList(headTitle);
-
-        mainpage_hotnews_vp_id.setOnBannerClickListener(new HeadBannerClickListener());
 
     }
 
@@ -210,6 +214,7 @@ public class Fragment_HotNews extends Fragment {
         }
     }
 
+
     private void setHeadData() {
 
         headTitle = new LinkedList<>();
@@ -222,79 +227,178 @@ public class Fragment_HotNews extends Fragment {
 
             headURL.add(mainNews_Head.get(i).getImgurl());
         }
+        mainpage_hotnews_vp_id.setImages(headURL);
+
+        mainpage_hotnews_vp_id.setBannerTitleList(headTitle);
+
+        mainpage_hotnews_vp_id.setOnBannerClickListener(new HeadBannerClickListener());
+
+    }
+
+    /**
+     * 设置listView
+     */
+    private void setListView() {
+
+        main_mainpage_hotnews_ptrlv_id.setAdapter(mainPageHotNewsAdapter);
+
+        main_mainpage_hotnews_ptrlv_id.setOnItemClickListener(new MainpageNewsItemClickListener());
+
+        main_mainpage_hotnews_ptrlv_id.setOnRefreshListener(mainpageOnRefreshListener);
+
+        main_mainpage_hotnews_ptrlv_id.setMode(PullToRefreshBase.Mode.BOTH);
     }
 
 
     /**
-     * 设置recyclerView
+     * 刷新监听
      */
-    private void setRecyclerView() {
-
-        main_mainpage_hotnews_rv_id.setAdapter(new MainPageHotNewsAdapter());
-
-        main_mainpage_hotnews_rv_id.setLayoutManager(new LinearLayoutManager(getContext()));
-
-    }
-
-
-    /**
-     * viewpager的适配器
-     */
-    private final class MainPageHotNewsAdapter extends RecyclerView.Adapter {
+    private final class MainpageOnRefreshListener implements PullToRefreshBase.OnRefreshListener2 {
 
         @Override
-        public int getItemViewType(int position) {
-
-            if (position == 0) {
-                return 0;
-            } else {
-                return 1;
-            }
+        public void onPullDownToRefresh(PullToRefreshBase refreshView) {
+            getNewsData();
         }
 
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-            View view = null;
-
-            RecyclerView.ViewHolder vh = null;
-
-            if (viewType == 0) {
-
-                vh = new MainPageHeadViewViewHolder(headView);
-
-            } else {
-
-                view = View.inflate(getContext(), R.layout.item_mainpage_hotnews, null);
-
-                vh = new MainPageHotNewsViewHolder(view);
-            }
-
-
-            return vh;
-        }
-
-        @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            if (holder instanceof MainPageHeadViewViewHolder) {
-            } else {
-                Picasso.with(getContext()).load(mainNews_List.get(position - 1).getImgurl()).into(((MainPageHotNewsViewHolder) holder).sircle_iv_id);
-                ((MainPageHotNewsViewHolder) holder).hot_news_tv_title_id.setText(mainNews_List.get(position - 1).getNewstitle());
-                ((MainPageHotNewsViewHolder) holder).hot_news_tv_time_id.setText(mainNews_List.get(position - 1).getPublishtime());
-            }
-        }
-
-        @Override
-        public int getItemCount() {
-            return mainNews_List.size() + 1;
+        public void onPullUpToRefresh(PullToRefreshBase refreshView) {
+            addNewsData();
         }
     }
 
 
     /**
-     * 一般条目的holder类
+     * 上拉加载更多
      */
-    private final class MainPageHotNewsViewHolder extends RecyclerView.ViewHolder {
+    private void addNewsData() {
+
+        HttpParams params = new HttpParams();
+        params.put("token", LocalInfo.token);
+        params.put("page", pageCount);
+        params.put("userid", LocalInfo.userid);
+        params.put("categoryid", 2);
+
+        kjhttp.post(UrlAddress.getURLAddress(UrlAddress.Url_GetHeadlineNews), params, false, new HttpCallBack() {
+            @Override
+            public void onSuccess(String t) {
+
+                try {
+                    String code = new JSONObject(t).getJSONObject("result").getString("error_code");
+                    if ("200".equals(code)) {
+                        pageCount++;
+                        addNewsData(t);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                super.onSuccess(t);
+
+            }
+
+            @Override
+            public void onFailure(int errorNo, String strMsg) {
+                Toast.makeText(getActivity(), "获取数据失败。。。", Toast.LENGTH_SHORT).show();
+                super.onFailure(errorNo, strMsg);
+            }
+
+            @Override
+            public void onFinish() {
+                main_mainpage_hotnews_ptrlv_id.onRefreshComplete();
+                super.onFinish();
+            }
+        });
+    }
+
+    /**
+     * 加载更多数据
+     *
+     * @param json
+     */
+    private void addNewsData(String json) {
+
+        List listTemp = new LinkedList();
+
+        try {
+
+            listTemp = JSONArray.parseArray(new JSONObject(json).getString("listnews"),
+                    Main_News_Head_Bean.class);
+
+            mainNews_List.addAll(listTemp);
+
+            mainPageHotNewsAdapter.notifyDataSetChanged();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 条目的点击监听器
+     */
+    private final class MainpageNewsItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            Intent intent = new Intent(getActivity(), Activity_News_Detail.class);
+
+            intent.putExtra("URL", mainNews_List.get(position));
+
+            startActivity(intent);
+
+        }
+    }
+
+
+    /**
+     * pulltorefreshlistview的适配器
+     */
+    private final class MainPageHotNewsAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mainNews_List.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mainNews_List.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            MainPageHotNewsViewHolder mainPageHotNewsViewHolder = null;
+
+            if (convertView == null) {
+                convertView = View.inflate(getContext(), R.layout.item_mainpage_hotnews, null);
+
+                mainPageHotNewsViewHolder = new MainPageHotNewsViewHolder(convertView);
+
+                convertView.setTag(mainPageHotNewsViewHolder);
+            } else {
+                mainPageHotNewsViewHolder = (MainPageHotNewsViewHolder) convertView.getTag();
+            }
+
+            Picasso.with(getContext()).load(mainNews_List.get(position).getImgurl()).into(mainPageHotNewsViewHolder.sircle_iv_id);
+            mainPageHotNewsViewHolder.hot_news_tv_title_id.setText(mainNews_List.get(position).getNewstitle());
+            mainPageHotNewsViewHolder.hot_news_tv_time_id.setText(mainNews_List.get(position).getPublishtime());
+
+            return convertView;
+        }
+    }
+
+
+    /**
+     * 条目的holder类
+     */
+    private final class MainPageHotNewsViewHolder {
 
         ImageView sircle_iv_id;
         TextView hot_news_tv_title_id;
@@ -302,40 +406,13 @@ public class Fragment_HotNews extends Fragment {
         TextView hot_news_tv_body_id;
 
         public MainPageHotNewsViewHolder(View itemView) {
-            super(itemView);
 
             sircle_iv_id = (ImageView) itemView.findViewById(R.id.sircle_iv_id);
             hot_news_tv_title_id = (TextView) itemView.findViewById(R.id.hot_news_tv_title_id);
             hot_news_tv_time_id = (TextView) itemView.findViewById(R.id.hot_news_tv_time_id);
             hot_news_tv_body_id = (TextView) itemView.findViewById(R.id.hot_news_tv_body_id);
 
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    Intent intent = new Intent(getActivity(), Activity_News_Detail.class);
-
-                    intent.putExtra("URL", mainNews_List.get(getAdapterPosition() - 1));
-
-                    startActivity(intent);
-                }
-            });
-
         }
     }
 
-
-    /**
-     * 头view 的holder类
-     */
-    private final class MainPageHeadViewViewHolder extends RecyclerView.ViewHolder {
-
-
-        public MainPageHeadViewViewHolder(View itemView) {
-            super(itemView);
-
-        }
-
-    }
 }
--- app/src/main/java/com/android/qdhhh/fakebeizhongyi/mainpage/childfragments/Fragment_Main_Alumni.java
+++ app/src/main/java/com/android/qdhhh/fakebeizhongyi/mainpage/childfragments/Fragment_Main_Alumni.java
@@ -10,6 +10,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +22,8 @@ import com.android.qdhhh.fakebeizhongyi.commom.LocalInfo;
 import com.android.qdhhh.fakebeizhongyi.commom.UrlAddress;
 import com.android.qdhhh.fakebeizhongyi.mainpage.Activity_News_Detail;
 import com.android.qdhhh.fakebeizhongyi.mainpage.bean.Main_News_Head_Bean;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
@@ -28,19 +32,22 @@ import org.kymjs.kjframe.KJHttp;
 import org.kymjs.kjframe.http.HttpCallBack;
 import org.kymjs.kjframe.http.HttpParams;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Fragment_Main_Alumni extends Fragment {
 
     private View view = null;
-    private SwipeRefreshLayout alumni_srl_id;
-    private RecyclerView alumni_rv_id;
+
+    private PullToRefreshListView alumni_ptrlv_id;
+
+    private int pageCount = 1;
 
     private List<Main_News_Head_Bean> listData;
 
     private KJHttp kjhttp;
 
-    private Alumni_RecyvlerView_Adapter alumni_RecyvlerView_Adapter;
+    private Alumni_ListView_Adapter alumni_ListView_Adapter;
 
     public Fragment_Main_Alumni() {
     }
@@ -49,15 +56,15 @@ public class Fragment_Main_Alumni extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
 
-        alumni_RecyvlerView_Adapter = new Alumni_RecyvlerView_Adapter();
-
         kjhttp = new KJHttp();
-
-        requestData();
 
         super.onCreate(savedInstanceState);
     }
 
+
+    /**
+     * 获取新数据
+     */
     private void requestData() {
 
         HttpParams params = new HttpParams();
@@ -74,6 +81,7 @@ public class Fragment_Main_Alumni extends Fragment {
                 try {
                     String code = new JSONObject(t).getJSONObject("result").getString("error_code");
                     if ("200".equals(code)) {
+                        pageCount = 2;
                         setData(t);
                     }
                 } catch (JSONException e) {
@@ -90,18 +98,26 @@ public class Fragment_Main_Alumni extends Fragment {
 
             @Override
             public void onFinish() {
-                alumni_srl_id.setRefreshing(false);
+                if (alumni_ptrlv_id.isRefreshing()) {
+                    alumni_ptrlv_id.onRefreshComplete();
+                }
                 super.onFinish();
             }
         });
     }
 
+
+    /**
+     * 设置数据
+     *
+     * @param string
+     */
     private void setData(String string) {
         try {
             listData = JSONArray.parseArray(new JSONObject(string).getString("focusnews"),
                     Main_News_Head_Bean.class);
 
-            alumni_RecyvlerView_Adapter.notifyDataSetChanged();
+            alumni_ListView_Adapter.notifyDataSetChanged();
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -115,8 +131,7 @@ public class Fragment_Main_Alumni extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_main_alumni, null);
 
-        alumni_srl_id = (SwipeRefreshLayout) view.findViewById(R.id.alumni_srl_id);
-        alumni_rv_id = (RecyclerView) view.findViewById(R.id.alumni_rv_id);
+        alumni_ptrlv_id = (PullToRefreshListView) view.findViewById(R.id.alumni_ptrlv_id);
 
         return view;
     }
@@ -124,76 +139,188 @@ public class Fragment_Main_Alumni extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 
-        alumni_srl_id.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                requestData();
-            }
-        });
+        listData = new LinkedList<>();
 
-        alumni_rv_id.setAdapter(alumni_RecyvlerView_Adapter);
-        alumni_rv_id.setLayoutManager(new GridLayoutManager(getContext(), 1));
+        alumni_ListView_Adapter = new Alumni_ListView_Adapter();
+
+        alumni_ptrlv_id.setAdapter(alumni_ListView_Adapter);
+
+        alumni_ptrlv_id.setOnItemClickListener(new AlumniOnItemClickListener());
+
+        alumni_ptrlv_id.setMode(PullToRefreshBase.Mode.BOTH);
+
+        alumni_ptrlv_id.setOnRefreshListener(new AlumniRefreshListener());
+
+        requestData();
+
         super.onActivityCreated(savedInstanceState);
     }
 
-    private final class Alumni_RecyvlerView_Adapter extends RecyclerView.Adapter {
+    /**
+     * 刷新监听器
+     */
+    private final class AlumniRefreshListener implements PullToRefreshBase.OnRefreshListener2 {
 
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-            View view = View.inflate(getContext(), R.layout.item_mainpage_hotnews, null);
-
-            Alumni_RecyvlerView_Holder vh = new Alumni_RecyvlerView_Holder(view);
-
-            return vh;
+        public void onPullDownToRefresh(PullToRefreshBase refreshView) {
+            requestData();
         }
 
-
         @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
-            Picasso.with(getContext()).load(listData.get(position).getImgurl()).into(((Alumni_RecyvlerView_Holder) holder).sircle_iv_id);
-            ((Alumni_RecyvlerView_Holder) holder).hot_news_tv_title_id.setText(listData.get(position).getNewstitle());
-            ((Alumni_RecyvlerView_Holder) holder).hot_news_tv_time_id.setText(listData.get(position).getPublishtime());
+        public void onPullUpToRefresh(PullToRefreshBase refreshView) {
+            addNewsData();
         }
+    }
+
+    /**
+     * 加载更多
+     */
+    private void addNewsData() {
+        HttpParams params = new HttpParams();
+
+        params.put("token", LocalInfo.token);
+        params.put("page", pageCount);
+        params.put("userid", LocalInfo.userid);
+        params.put("categoryid", 2);
 
 
-        @Override
-        public int getItemCount() {
-            if (listData == null) {
-                return 0;
+        kjhttp.post(UrlAddress.getURLAddress(UrlAddress.Url_GetGeneralNews), params, false, new HttpCallBack() {
+            @Override
+            public void onSuccess(String t) {
+                try {
+                    String code = new JSONObject(t).getJSONObject("result").getString("error_code");
+                    if ("200".equals(code)) {
+                        pageCount++;
+                        addData(t);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                super.onSuccess(t);
             }
-            return listData.size();
+
+            @Override
+            public void onFailure(int errorNo, String strMsg) {
+                Toast.makeText(getActivity(), "获取数据失败。。。", Toast.LENGTH_SHORT).show();
+                super.onFailure(errorNo, strMsg);
+            }
+
+            @Override
+            public void onFinish() {
+                if (alumni_ptrlv_id.isRefreshing()) {
+                    alumni_ptrlv_id.onRefreshComplete();
+                }
+                super.onFinish();
+            }
+        });
+    }
+
+    /**
+     * 添加新数据
+     *
+     * @param t
+     */
+    private void addData(String t) {
+
+        List<Main_News_Head_Bean> list = new LinkedList();
+
+        try {
+            list = JSONArray.parseArray(new JSONObject(t).getString("focusnews"),
+                    Main_News_Head_Bean.class);
+
+            listData.addAll(list);
+
+            alumni_ListView_Adapter.notifyDataSetChanged();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 
 
-    private final class Alumni_RecyvlerView_Holder extends RecyclerView.ViewHolder {
+    /**
+     * 条目点击监听器
+     */
+    private final class AlumniOnItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            Intent intent = new Intent(getActivity(), Activity_News_Detail.class);
+
+            intent.putExtra("URL", listData.get(position));
+
+            startActivity(intent);
+
+        }
+    }
+
+
+    /**
+     * 刷新ListView适配器
+     */
+    private final class Alumni_ListView_Adapter extends BaseAdapter {
+
+
+        @Override
+        public int getCount() {
+            return listData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return listData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            Alumni_ListView_Holder alumni_listView_holder = null;
+
+            if (convertView == null) {
+
+                convertView = View.inflate(getContext(), R.layout.item_mainpage_hotnews, null);
+
+                alumni_listView_holder = new Alumni_ListView_Holder(convertView);
+
+                convertView.setTag(alumni_listView_holder);
+
+            } else {
+
+                alumni_listView_holder = (Alumni_ListView_Holder) convertView.getTag();
+
+            }
+            Picasso.with(getContext()).load(listData.get(position).getImgurl()).into(alumni_listView_holder.sircle_iv_id);
+            alumni_listView_holder.hot_news_tv_title_id.setText(listData.get(position).getNewstitle());
+            alumni_listView_holder.hot_news_tv_time_id.setText(listData.get(position).getPublishtime());
+
+            return convertView;
+        }
+    }
+
+
+    /**
+     * viewholder实体类
+     */
+    private final class Alumni_ListView_Holder {
 
         ImageView sircle_iv_id;
         TextView hot_news_tv_title_id;
         TextView hot_news_tv_time_id;
         TextView hot_news_tv_body_id;
 
-        public Alumni_RecyvlerView_Holder(View itemView) {
-            super(itemView);
+        public Alumni_ListView_Holder(View itemView) {
 
             sircle_iv_id = (ImageView) itemView.findViewById(R.id.sircle_iv_id);
             hot_news_tv_title_id = (TextView) itemView.findViewById(R.id.hot_news_tv_title_id);
             hot_news_tv_time_id = (TextView) itemView.findViewById(R.id.hot_news_tv_time_id);
             hot_news_tv_body_id = (TextView) itemView.findViewById(R.id.hot_news_tv_body_id);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    Intent intent = new Intent(getActivity(), Activity_News_Detail.class);
-
-                    intent.putExtra("URL", listData.get(getAdapterPosition()));
-
-                    startActivity(intent);
-                }
-            });
         }
     }
 }
